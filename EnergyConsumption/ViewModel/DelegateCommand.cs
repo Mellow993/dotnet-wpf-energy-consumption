598f14f95244d2ce5b6678aac47914e3fdd986e1
mellow <stay.mellow.993@gmail.com>
2022-04-16 20:41:49 +0200

    Implement viewmodel template

---
+++ EnergyConsumption/ViewModel/DelegateCommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace EnergyConsumption.ViewModel
+{
+    internal class DelegateCommand : ICommand
+    {
+        private Action _execute;
+        private Func<bool> _canExecute;
+        public event EventHandler? CanExecuteChanged;
+        public DelegateCommand(Action execute) { _execute = execute; }
+        public DelegateCommand(Action execute, Func<bool> canexecute) { _execute = execute; _canExecute = canexecute; }
+        public void OnExecuteChanged() => CanExecuteChanged?.Invoke(this, new EventArgs());
+        public bool CanExecute(object? parameter) => _canExecute != null ? _canExecute() : true;
+        public void Execute(object? parameter) => _execute();
+    }
+}
---
+++ EnergyConsumption/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnergyConsumption.ViewModel
+{
+
+    internal class MainWindowViewModel : ViewModelBase
+    {
+        #region Fields
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Events
+        public event EventHandler firstEvent;
+        public event EventHandler secondEvent;
+        public event EventHandler thridEvent;
+        #endregion
+        #region DelegateCommands
+        public DelegateCommand AddCommand { get; private set; }
+        public DelegateCommand RemoveCommand { get; private set; }
+        public DelegateCommand SaveAsCSVCommand { get; private set; }
+        public DelegateCommand StoreInDatabaseCommand { get; private set; }
+        public DelegateCommand ExitCommand { get; private set; }
+        #endregion
+
+        public MainWindowViewModel()
+        {
+            SetUpDelegateCommands();
+        }
+
+        private void SetUpDelegateCommands()
+        {
+            AddCommand = new DelegateCommand(AddInformation, CanAddInformation);
+            RemoveCommand = new DelegateCommand(RemoveInformation, CanRemoveInformation);
+            StoreInDatabaseCommand = new DelegateCommand(StoreInDatabase, CanStoreInDatabase);
+            SaveAsCSVCommand = new DelegateCommand(SaveAsCSV, CanSaveAsCSV);
+            ExitCommand = new DelegateCommand(Exit, CanExit);
+
+        }
+
+        #region MyRegion
+        private void AddInformation()
+        {
+            throw new NotImplementedException();
+        }        
+        
+        private void RemoveInformation()
+        {
+            throw new NotImplementedException();
+        }      
+        
+        private void StoreInDatabase()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SaveAsCSV()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Exit()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region CanExecute
+        private bool CanAddInformation() => true;
+        private bool CanRemoveInformation() => true;
+        private bool CanStoreInDatabase() => true;
+        private bool CanSaveAsCSV() => true;
+        private bool CanExit() => true;
+        #endregion
+    }
+}
